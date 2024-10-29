d26b4f170eaf8a386245c93576a187a96aad0254
Chaizee <zuzindima26@gmail.com>
2024-10-29 14:00:24 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Lab
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int prev = int.Parse(Console.ReadLine());
+        int cur = int.Parse(Console.ReadLine());
+        int LocalMin = 0;
+        for (int i = 2; i < n; i++)
+        {
+            int next = int.Parse(Console.ReadLine());
+            if (cur < prev && cur < next)
+            {
+                LocalMin++;
+            }
+            prev = cur;
+            cur = next;
+        }
+        Console.WriteLine(LocalMin);
+
+    }
+
+}
+
+
