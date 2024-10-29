08929243da86875c64ded33e79b41a50f7ac5000
Chaizee <zuzindima26@gmail.com>
2024-10-29 14:06:53 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+// Определить количество локальных минимумов﻿
 class Lab
 {
     public static void Main(string[] args)
@@ -18,9 +18,34 @@ class Lab
             cur = next;
         }
         Console.WriteLine(LocalMin);
-
     }
-
+}
+//Определить все ли элементы последовательности чётные
+class Program {
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int prev = int.Parse(Console.ReadLine());
+        int Chet = 1;
+        for (int i = 1; i < n; i++)
+        {
+            int next = int.Parse(Console.ReadLine());
+            if ((prev % 2) == 0)
+            {
+                Chet++;
+            }        
+            prev = next;
+        }
+        if (Chet == n)
+        {
+            Console.WriteLine("Все эллементы четные");
+        }
+        else
+        {
+            Console.WriteLine("Не все эллементы четные");
+        }
+    }
 }
 
-
+//Необходимо определить два максимума
+    
